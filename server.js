077d848a7ba8823b01516af9a22953ec748004ec
SenorPenguin <jimmy.dwyer25@gmail.com>
2019-01-23 18:52:21 -0600

    initialized repo, set up folder structure, set up simple index loop. Luv u.

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(methodOverride('_method'));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT);
+
+
