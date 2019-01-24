2da58de1e9c07297dbb92cd13baf13324a0c62f1
SenorPenguin <jimmy.dwyer25@gmail.com>
2019-01-23 19:00:16 -0600

    finished tiny bits of middleware/exporting/importing that is necessary. Need to study this code over and over as it's static and always used in this way.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var exphbs = require('express-handlebars');
 
 var PORT = process.env.PORT || 3000;
 
+var routes = require('./controllers/burgersController')
+
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
@@ -14,6 +16,8 @@ app.use(methodOverride('_method'));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.listen(PORT);
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+  });
 
-
+app.use('/',routes);
