596c6f4071299d1ddeb3bf1e1fc5250f599bbc05
SenorPenguin <jimmy.dwyer25@gmail.com>
2019-01-26 12:38:22 -0600

    figured out passing of info named in burgerscontroller was not named properly in index.handlebars. This makes me want to drown puppies.

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -3,15 +3,18 @@ var express = require('express');
 //sets up the routing ability of express? Maybe? Need clarity
 var router = express.Router();
 //imports burger.js now that it's written
-var burger = require('../models/burger.js')
+var burger = require('../models/burger.js');
 
 //lets the virtual path be established
 router.get('/', function(req, res){
     burger.all(function(burger_data){
-        console.log(burger_data);
-        res.render('index');
-    })
-})
+        var hbsObject = {
+            burgers: burger_data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 //links the update 
 router.put('/burgers/update', function(req, res){
@@ -25,7 +28,7 @@ router.post('burgers/create', function(req,res){
     burger.create(req.body.burger_name, function(result){
         console.log(result);
         res.redirect('/');
-    })
+    });
 })
 
 //as well as importing needs, lines 6&7 need this to be exported
